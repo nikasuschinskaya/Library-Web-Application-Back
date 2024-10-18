27b1f1ae0ef21f464a27324d034a763139ce6014
nikasuschinskaya@gmail.com <nikasuschinskaya@gmail.com>
2024-10-18 03:05:17 +0300

    some corrects

--- src/Library/Library.Api/Program.cs
+++ src/Library/Library.Api/Program.cs
@@ -22,10 +22,8 @@ var builder = WebApplication.CreateBuilder(args);
         options.UseSqlServer(connectionString);
     });
 
-    // Регистрация UnitOfWork
     builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-    // Регистрация репозиториев
     builder.Services.AddScoped(typeof(IRepository<>), typeof(EntityFrameworkRepository<>));
 
     builder.Services.AddSingleton<IInitializer<Role>, RoleInitializer>();
@@ -79,82 +77,3 @@ var app = builder.Build();
 
     app.Run();
 }
-
-
-//var builder = WebApplication.CreateBuilder(args);
-//{
-//    builder.Services.AddControllers();
-
-//    builder.Services.AddEndpointsApiExplorer();
-//    builder.Services.AddSwaggerGen();
-
-//    builder.Services.AddDbContext<LibraryDbContext>(options =>
-//    {
-//        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//        options.UseSqlServer(connectionString);
-//    });
-
-//    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-//    builder.Services.AddScoped(typeof(IRepository<>), typeof(EntityFrameworkRepository<>));
-
-
-//    builder.Services.AddSingleton<IInitializer<Role>, RoleInitializer>();
-//    builder.Services.AddScoped<DbContextInitializer>((provider) =>
-//    {
-//        var roleInitializer = provider.GetRequiredService<IInitializer<Role>>();
-//        var initializer = new DbContextInitializer(roleInitializer);
-//        var unitOfWork = provider.GetRequiredService<IUnitOfWork>();
-
-//        initializer.Initialize(unitOfWork);
-//        return initializer;
-//    });
-
-//    builder.Services.AddFluentValidationAutoValidation();
-//    builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
-//    builder.Services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>();
-
-//    builder.Services.AddCors();
-
-//    builder.Services.AddAuthorizationBuilder()
-//        .AddPolicy("Admin", policy => policy.RequireRole("Admin"))
-//        .AddPolicy("User", policy => policy.RequireRole("User"));
-//}
-
-
-//var app = builder.Build();
-//{
-//    using var scope = app.Services.CreateScope();
-//    var initializer = scope.ServiceProvider.GetRequiredService<DbContextInitializer>();
-
-//    if (app.Environment.IsDevelopment())
-//    {
-//        app.UseSwagger();
-//        app.UseSwaggerUI();
-//    }
-
-//    app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
-
-//    app.UseCors();
-//    //app.UseCors(builder =>
-//    //{
-//    //    builder.WithOrigins("http://localhost:5173")
-//    //           .AllowAnyHeader()
-//    //           .AllowAnyMethod()
-//    //           .AllowCredentials();
-//    //});
-
-//    app.UseStaticFiles();
-
-//    app.UseRouting();
-
-//    app.UseAuthentication();
-
-//    app.UseHttpsRedirection();
-
-//    app.UseAuthorization();
-
-//    app.MapControllers();
-
-//    app.Run();
-//}
--- src/Library/Library.Api/obj/Debug/net8.0/Library.Api.AssemblyInfo.cs
+++ src/Library/Library.Api/obj/Debug/net8.0/Library.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8fe090ca0baa013cc7393d2bec20767dff24eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4924acfaadc37ba15c4333c3a428bdc7647847")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
